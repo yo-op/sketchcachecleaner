2819a12882ce0d8f4c531965f6c289aa510c8350
minikin <djminikin@gmail.com>
2020-03-29 15:46:53 +0200

    Add Sentry

--- Sketch Cache Cleaner/AppDelegate.swift
+++ Sketch Cache Cleaner/AppDelegate.swift
@@ -7,11 +7,21 @@
 //
 
 import Cocoa
+import Sentry
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
+    configureSentry()
   }
-}
+
+    fileprivate func configureSentry() {
+        do {
+            Client.shared = try Client(dsn: "https://90daa0afc9e04716a579a237e92d021b@sentry.io/5179181")
+            try Client.shared?.startCrashHandler()
+        } catch let error {
+            print(error)
+        }
+    }
+ }
